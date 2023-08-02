a51adfe9f8cbf0d1c86706bf3b04b66ef09f4a10
SonishMaharjanfuse <sonish@fusemachines.com>
2023-08-02 17:51:19 +0545

    feat: commmit to check the valid commit

--- addition.py
+++ addition.py
@@ -1,11 +1,17 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return(a-b)
-def division(a,b):
-    return a/b
-def multiply(a,b):
-    return a*b
+def add(a, b):
+    return a + b
 
 
-print("pre-commit5")
+def sub(a, b):
+    return a - b
+
+
+def division(a, b):
+    return a / b
+
+
+def multiply(a, b):
+    return a * b
+
+
+print("check commit message ")
